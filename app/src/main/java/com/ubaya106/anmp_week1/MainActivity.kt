09be79ef184ef70491fae4b16809831a8de7db4b
Zufar Rachman <zufarafi02#gmail.com>
2023-08-28 14:04:46 +0700

    kutu kuda laut

---
+++ app/src/main/java/com/ubaya106/anmp_week1/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.ubaya106.anmp_week1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //CREATED BY ZUFAR
+    }
+}
